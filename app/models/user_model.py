3b5691b4a9369289f714a2cd65e270d42f796b8b
kaorikizuna <34364395+Agung-Krisna@users.noreply.github.com>
2024-10-13 16:48:06 +0800

    feat: user related models

---
+++ app/models/user_model.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+import pydantic
+from app.models.request_model import Coordinates
+from typing import Optional
+from app.config import password_context
+
+
+class User(BaseModel):
+    """
+    Main user data, containing all data retrieved from database
+    """
+    # authentication purposes
+    email: pydantic.EmailStr
+    password: str
+
+    # user information
+    full_name: pydantic.constr(max_length=50)
+    coordinates: Optional[Coordinates] = None
+
+
+class UserRegister(BaseModel):
+    email: pydantic.EmailStr
+    password: pydantic.constr(min_length=8)
+
+    full_name: pydantic.constr(max_length=50)
+
+    @pydantic.field_validator("password")
+    @classmethod
+    def hash_password(cls, value: str) -> str:
+        return password_context.hash(value)
+
+
+class UserLogin(BaseModel):
+    email: pydantic.EmailStr
+    password: str
