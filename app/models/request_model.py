7f5c3fc6a573509eeb2fa916c2a2a69f1eb78706
kaorikizuna <34364395+Agung-Krisna@users.noreply.github.com>
2024-10-26 04:53:52 +0800

    feat: changing ValidationError to ValueError to align with FastAPI's error handling

--- app/models/request_model.py
+++ app/models/request_model.py
@@ -15,14 +15,14 @@ class FrameRequest(BaseModel):
         try:
             image_bytes = base64.b64decode(value)
         except (Exception, binascii.Error):
-            raise ValidationError("Invalid base64 code")
+            raise ValueError("Invalid base64 code")
         try:
             image = Image.open(io.BytesIO(image_bytes))
             image.copy().verify()  # verify header
             image.copy().load()  # load pixel data into memory (prevent truncated images)
             image.convert("RGB")  # check if can be decoded with RGB scheme
         except (IOError, OSError):
-            raise ValidationError("Not an image")
+            raise ValueError("Not an image")
         return image_bytes
 
 
@@ -33,11 +33,21 @@ class Points(BaseModel):
 
 class Coordinates(BaseModel):
     # set minimum coordinate length here
-    source_coordinates: conlist(Points, min_length=4)
-    destination_coordinates: conlist(Points, min_length=4)
+    source_coordinates: conlist(Points, min_length=4, max_length=20)
+    destination_coordinates: conlist(Points, min_length=4, max_length=20)
 
     @model_validator(mode='after')
     def check_equal_length(self):
         if len(self.source_coordinates) != len(self.destination_coordinates):
-            raise ValidationError('source coordinates and destination coordinates must have the same length')
+            raise ValueError('source coordinates and destination coordinates must have the same length')
+        return self
+
+    @model_validator(mode='after')
+    def check_all_zeros(self):
+        if self.source_coordinates is None and self.destination_coordinates is None:
+            return self
+        if all(src_coord.x == 0 and src_coord.y == 0 for src_coord in self.source_coordinates):
+            raise ValueError("All source coordinates cannot have zero values")
+        if all(dst_coord.x == 0 and dst_coord.y == 0 for dst_coord in self.destination_coordinates):
+            raise ValueError("All destination coordinates cannot have zero values")
         return self
