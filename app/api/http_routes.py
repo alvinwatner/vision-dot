f2c7b8108be9f9cefefa9846c360c0e1b26664a5
kaorikizuna <34364395+Agung-Krisna@users.noreply.github.com>
2024-10-26 04:53:02 +0800

    feat: serving the HTML interface for the app

--- app/api/http_routes.py
+++ app/api/http_routes.py
@@ -1,10 +1,12 @@
-from fastapi import Security, Response, APIRouter, HTTPException, status
+from fastapi import Security, Response, APIRouter, HTTPException, status, Request
+from fastapi.templating import Jinja2Templates
 from app.config import access_security
 from app.models.response_model import SuccessResponse, ErrorResponse
 from app.models.request_model import Coordinates
 from fastapi_jwt import JwtAuthorizationCredentials
 from app.database import users_collection
 from app.models.user_model import User
+from app.config import cache, project_root
 
 router = APIRouter()
 
@@ -15,12 +17,15 @@ async def set_coordinates(coordinates: Coordinates,
     user_email = credentials.subject.get("email")
     user = await users_collection.find_one({"email": user_email})
     user = User(**user)
-    print(user)
     user.coordinates = coordinates
 
     try:
         update_query = {"$set": user.dict()}
         result = await users_collection.update_one({"email": user_email}, update_query)
+
+        # cache the coordinates to reduce database calls
+        cache[user_email] = user.coordinates
+
         if result.modified_count == 0:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                 detail=ErrorResponse(message="User not found or no changes were made").dict())
@@ -28,3 +33,14 @@ async def set_coordinates(coordinates: Coordinates,
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=ErrorResponse(message="Unknown error", error=str(e)).dict())
+
+
+templates = Jinja2Templates(directory=project_root / "templates")
+
+
+@router.get("/homographic_transformation")
+async def homograhpic_transormation(request: Request,
+                                    credentials: JwtAuthorizationCredentials = Security(access_security)):
+    websocket_url = request.url_for("websocket_homographic")
+
+    return templates.TemplateResponse("example_frontend.html", context={"request": request, "websocket_url": websocket_url})
