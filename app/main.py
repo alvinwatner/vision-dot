06897f16afafc42bf604df6d7294f522dd6735d8
kaorikizuna <34364395+Agung-Krisna@users.noreply.github.com>
2024-11-08 11:17:21 +0800

    feat: added CORS configuration to allow all origins.

--- app/main.py
+++ app/main.py
@@ -5,12 +5,21 @@ from app.api.websocket_routes import router as websocket_routes
 from app.api.auth_routes import router as auth_routes
 from app.api.http_routes import router as http_routes
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(websocket_routes)
 app.include_router(auth_routes)
 app.include_router(http_routes)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def check_registered_routes():
